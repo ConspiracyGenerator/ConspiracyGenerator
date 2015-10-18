4849331551c5eaff945e6818a625f8e442405a80
Dennis Orzikh <orzikhd@uw.edu>
2015-10-17 21:35:54 -0700

    event handlers

--- conspiracyGenerator.js
+++ conspiracyGenerator.js
@@ -1,3 +1,13 @@
 window.onload = function() {
+	//alert("hello world");
+	document.getElementById("input").onchange = enableSubmit;
+	document.getElementById("submit").onclick = processInput;
+}
+
+enableSubmit = function() {
+	document.getElementById("submit").disabled = false;
+}
+
+processInput = function() {
 	alert("hello world");
 }
